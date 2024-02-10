869872d68d0ad31a044f9b6a62218332fe5c825e
Athos <athosg05@gmail.com>
2024-02-10 10:35:23 -0300

    adiconando

--- Exerciios/aula 12ex/ex015/script.js
+++ Exerciios/aula 12ex/ex015/script.js
@@ -1,13 +1,14 @@
 
-function Verificar() {
+function verificar() {
     var data = new Date()
-    var ano = data.getFullYear
+    var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
-    var res = document.getElementById('res')
-    if (fano.value == 0 || Number(fano.value) > ano) {
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || fano.value < ano) {
         window.alert('[ERROR]Verifique se os Dados estao certos')
-        
+      
     } else {
+        
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero = ''
