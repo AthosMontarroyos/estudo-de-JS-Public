ee15059647f10ef41865e7d061ae1a88258b9024
Athos <athosg05@gmail.com>
2024-02-10 10:40:14 -0300

    concertando Error

--- Exerciios/aula 12ex/ex015/script.js
+++ Exerciios/aula 12ex/ex015/script.js
@@ -4,7 +4,7 @@ function verificar() {
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
-    if (fano.value.length == 0 || fano.value < ano) {
+    if (fano.value.length == 0 || fano.value > ano) {
         window.alert('[ERROR]Verifique se os Dados estao certos')
       
     } else {
