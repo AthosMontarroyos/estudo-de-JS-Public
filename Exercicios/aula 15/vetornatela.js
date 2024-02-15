b4980e88c2a7cb1d544833e340feb2589e7a026e
Athos <athosg05@gmail.com>
2024-02-15 14:40:50 -0300

    comecei exercio 07

--- Exercicios/aula 15/vetornatela.js
+++ Exercicios/aula 15/vetornatela.js
@@ -1,10 +1,13 @@
 let num = [5 , 8, 2, 9, 3]
+var res = 'div#res'
 num.sort()
 /*for(let pos=0; pos < num.length ; pos++) {
     console.log(` a posição ${pos} tem o valor ${num[pos]}`)
 
 }*/
 
-for(let pos in num) {
+/*for(let pos in num) {
     console.log(`a posição ${pos} tem o valor ${num[pos]}`)
-}
+}*/
+
+
